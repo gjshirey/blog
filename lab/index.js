208819b597538d225d73fd8809a4f759f1a240f5
Galen Shirey <galenshirey@gmail.com>
2020-02-27 11:00:21 -0500

    Updated index.js

--- lab/index.js
+++ lab/index.js
@@ -1,7 +1,15 @@
 const http = require('http')
 const server = http.createServer((req,res) => {
-    console.log(req.url)
-    res.end('Hello Node.js')
+    if(req.url === '/about')
+        res.end('The About Page')
+    else if(req.url === '/contact')
+        res.end('The Contact Page')
+    else if(req.url === '/')
+        res.end('The Home Page')
+    else{
+        res.writeHead(404)
+        res.end('page not found')
+    }
 })
 
 server.listen(3000)
