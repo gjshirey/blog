4c7e7117664e3b63bd6796dc168997c233b5cc06
Galen Shirey <galenshirey@gmail.com>
2020-03-20 10:36:08 -0400

    Added Express

--- lab/index.js
+++ lab/index.js
@@ -1,20 +1,21 @@
 
 const express = require('express')
 const path = require('path')
-
 const app = express() //call express function to start new Express app
-app.listen(3000, () => {
+app.use(express.static('public'))
+
+app.listen(3000, () => { //Listen on port 3000
     console.log("App listening on port 3000")
 })
 
-app.get('/', (req,res)=>{
+app.get('/', (req,res)=>{ // localhost:3000/ is index.html
     res.sendFile(path.resolve(__dirname, 'index.html'))
 })
 
-app.get('/about', (req,res)=>{
+app.get('/about', (req,res)=>{ // localhost:3000/about is about.html
     res.sendFile(path.resolve(__dirname, 'about.html'))
 })
 
-app.get('/contact', (req,res)=>{
+app.get('/contact', (req,res)=>{ // localhost:3000/contact is contact.html
     res.sendFile(path.resolve(__dirname, 'contact.html'))
 })
