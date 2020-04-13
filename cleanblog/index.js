c7093207ac78bc175f2ca3cfb7f3d096b46d1524
gjshirey <galenshirey@gmail.com>
2020-04-13 10:47:21 -0400

    ch3-8

--- cleanblog/index.js
+++ cleanblog/index.js
@@ -1,35 +1,96 @@
 const express = require('express')
 const path = require('path')
 const mongoose = require('mongoose')
+const fileUpload = require('express-fileupload')
+const ejs = require('ejs')
 
 const app = new express()
-const ejs = require('ejs')
 
 app.set('view engine', 'ejs')
 app.use(express.static('public'))
-mongoose.connect('mongodb+srv://gshir326:123456-@cluster0-w5zfz.mongodb.net/test?retryWrites=true&w=majority')
+mongoose.connect('mongodb+srv://gshir326:123456-@cluster0-w5zfz.mongodb.net/test?retryWrites=true&w=majority', { useNewUrlParser: true, useUnifiedTopology: true })
 
-app.listen(4000, () => {
+//Body Parser
+const bodyParser = require('body-parser')
+app.use(bodyParser.json())
+app.use(bodyParser.urlencoded({ extended: true }))
+app.use(fileUpload())
+
+
+//Custom test middleware
+//Validates all fields of post are filled
+const validateMiddleWare = (req, res, next) => {
+    if (req.files == null || req.body.title == null || req.body.title == null) {
+        return res.redirect('/post/new')
+    }
+    next()
+}
+app.use('/posts/store', validateMiddleWare)
+
+
+//BlogPost Model
+const BlogPost = require('./models/BlogPost.js')
+
+//for heroku deployment
+const PORT = process.env.PORT || 4000;
+
+app.listen(PORT, () => {
     console.log('App listening on port 4000')
 })
 
-app.get('/', (req, res) => {
-    //res.sendFile(path.resolve(__dirname, 'pages/index.html'))
-    res.render('index');
+
+//Main Page
+app.get('/', async (req, res) => {
+    const blogposts = await BlogPost.find({})
+    res.render('index', {
+        blogposts
+    });
 })
-app.get('/about', (req, res) => {
+
+//About Page
+app.get('/about', async (req, res) => {
     //res.sendFile(path.resolve(__dirname, 'pages/about.html'))
-    res.render('about');
+    const blogposts = await BlogPost.find({})
+    res.render('about', {
+        blogposts
+    });
 })
-app.get('/contact', (req, res) => {
+
+//Contact Page
+app.get('/contact', async (req, res) => {
     //res.sendFile(path.resolve(__dirname, 'pages/contact.html'))
-    res.render('contact');
+    const blogposts = await BlogPost.find({})
+    res.render('contact', {
+        blogposts
+    });
 })
-app.get('/post', (req, res) => {
-    //res.sendFile(path.resolve(__dirname, 'pages/post.html'))
-    res.render('post');
+
+//Post Page
+app.get('/post/:id', async (req, res) => {
+    const blogpost = await BlogPost.findById(req.params.id)
+    res.render('post', {
+        blogpost
+    });
+
 })
-app.get('/post/new', (req, res) => {
+//new post page
+app.get('/posts/new', (req, res) => {
     //res.sendFile(path.resolve(__dirname, 'pages/post.html'))
     res.render('create');
 })
+
+//create post
+app.post('/posts/store', (req, res) => {
+    let image = req.files.image;
+    image.mv(path.resolve(__dirname, 'public/img', image.name), async (error) => {
+        await BlogPost.create({
+            ...req.body,
+            image: '/img/' + image.name
+        })
+        res.redirect('/')
+    })
+})
+
+
+
+
